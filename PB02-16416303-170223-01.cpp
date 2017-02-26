9e3eb134e4be8ca78724c907737a6dda54af8bcd
Jaya Kusuma <kusumavictor@yahoo.co.id>
2017-02-26 19:18:41 +0700

    Update PB02-16416303-170223-01.cpp

--- PB02-16416303-170223-01.cpp
+++ PB02-16416303-170223-01.cpp
@@ -1,69 +1,65 @@
-// NIM/Nama : 16416303
-// Nama file: PB02-16416303-170223-01.cpp
-// Tanggal  : 23 Febuari 2017
-// Deskripsi: program permainan ambil batu
 #include <iostream>
 using namespace std;
 
 int main(){
-    //deklarasi variabel
+
     int batuawal, batu1, batu2, batutotal, batusave1, batusave2, turn1, turn2;
 
 
-    cout << "masukkan batu awal" << "\n";
+    cout << "specify how much stones are available" << "\n";
     cin >> batuawal;
 
-    //looping utama
+
     while(batuawal>=0){
-        //set boolean sebagai parameter loop validasi
+
         turn1 = false;
         turn2 = false;
 
         //giliran pemain 1
         while(turn1 == false){
-            cout << "Masukan jumlah batu yang diambil pemain 1 : " << "\n";
+            cout << "Choose how many stones player 1 want to take : " << "\n";
             cin >> batu1;
 
             //validasi input
             if(batu1 > 4 || batu1 < 1){
-                cout << "Jumlah batu harus berada diantara 1 dan 4!" << "\n";
+                cout << "The picked stones must between 1 and 4" << "\n";
 
             }else if(batuawal < batu1){
-                cout << "Banyak batu yang tersisa lebih sedikit dibanding batu yang diambil :(" << "\n";
+                cout << "There is not enough stones available to take :(" << "\n";
 
             }else{
                 batuawal = batuawal-batu1;
                 batusave1 = batusave1 + batu1;
-                cout << "Banyak batu tersisa : " << batuawal << "\n";
+                cout << "Stones available : " << batuawal << "\n";
                 turn1 = true;
             };
         }
 
-        //cek jumlah batu yang masih tersisa
+
         if(batuawal == 0){
           if(batusave1 < batusave2){
-            cout << "pemain 2 menang!" << "\n";
+            cout << "player 2 won!" << "\n";
           }else{
-            cout << "pemain 1 menang!" << "\n";
+            cout << "player 1 won!" << "\n";
           }
           break;
         };
 
-        //giliran pemain 2
+        
         while(turn2 == false){
-            cout << "Masukan jumlah batu yang diambil pemain 2 : " << "\n";
+            cout << "Choose how many stones player 2 want to take : " << "\n";
             cin >> batu2;
             //validasi input
             if(batu2 > 4 || batu2 < 1){
-                cout << "Jumlah batu harus berada diantara 1 dan 4!" << "\n";
+                cout << "The picked stones must between 1 and 4" << "\n";
 
             }else if(batuawal < batu2){
-                cout << "Banyak batu yang tersisa lebih sedikit dibanding batu yang diambil :(" << "\n";
+                cout << "There is not enough stones available to take :(" << "\n";
 
             }else{
                 batuawal = batuawal-batu2;
                 batusave2 = batusave2 + batu2;
-                cout << "Banyak batu tersisa : " << batuawal << "\n";
+                cout << "Stones available : " << batuawal << "\n";
                 turn2 = true;
             };
         }
